8fd15a304d44f27990f9a172131f6f4549b422e6
Travis Manning <travis.manning1@gmail.com>
2024-08-12 15:26:42 -0400

    test

--- inline_procs/inline_test_proc.sql
+++ inline_procs/inline_test_proc.sql
@@ -1,9 +1,9 @@
-create or replace procedure test_proc (input varchar) returns varchar
+create or replace procedure inline_test_proc (input varchar) returns varchar
   language python
   runtime_version = '3.11'
   handler = 'main'
   packages = ('snowflake-snowpark-python')
 as $$
 def main(input):
-  return f'Input value was "{input}"'
+  return f'Inline input value was "{input}"'
 $$;
--- procs/other_test_proc.sql
+++ procs/other_test_proc.sql
@@ -2,6 +2,7 @@ create or replace procedure other_test_proc (input varchar) returns varchar
   language python
   runtime_version = '3.11'
   handler = 'main'
+  packages = ('snowflake-snowpark-python')
 as $$
 def main(input):
   return f'Input value was "{input}"'
