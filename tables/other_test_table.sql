bbab1ea339c2d0bccc04b3cb776e0605384f64d2
Travis Manning <travis.manning1@gmail.com>
2024-08-14 12:24:41 -0400

    Update other_test_table.sql

--- tables/other_test_table.sql
+++ tables/other_test_table.sql
@@ -2,5 +2,6 @@ create or alter table other_test_table
 (
   id int not null identity primary key,
   value varchar not null,
-  salary number null
+  salary number null,
+  load_date timestamp default current_timestamp()
 );
