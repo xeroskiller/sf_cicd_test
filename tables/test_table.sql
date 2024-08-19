3f415c359b7d1913e87a65f5b0bcc1a4093444b7
Travis Manning <travis.manning1@gmail.com>
2024-08-19 11:20:37 -0400

    Update test_table.sql

--- tables/test_table.sql
+++ tables/test_table.sql
@@ -3,5 +3,6 @@ create or alter table test_table
   id int not null identity primary key,
   value varchar not null,
   salary number null,
-  load_date timestamp default current_timestamp()
+  load_date timestamp default current_timestamp(),
+  json_payload variant null
 );
